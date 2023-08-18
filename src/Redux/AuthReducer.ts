bdc47d10a8e19a2185bbba3fbdef42310ebe296d
Sandip7677 <anuj7870040353@gmail.com>
2023-08-18 15:47:57 +0530

    redux store created for user info

---
+++ src/Redux/AuthReducer.ts
@@ -0,0 +1,31 @@
+// authReducer.ts
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface User {
+  username: string;
+  // Other user properties
+}
+
+interface AuthState {
+  user: User | null;
+}
+
+const initialState: AuthState = {
+  user: null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    login: (state, action: PayloadAction<User>) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
---
+++ src/Redux/store.ts
@@ -0,0 +1,19 @@
+// store.ts
+import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import authReducer from './AuthReducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+export default store;
