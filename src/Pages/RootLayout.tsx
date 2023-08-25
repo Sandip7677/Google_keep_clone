365cedb3dedb392efd943d9afd5af092b1ea2dae
Sandip7677 <anuj7870040353@gmail.com>
2023-08-25 13:17:45 +0530

    updated navigation ui and style

--- src/Pages/RootLayout.tsx
+++ src/Pages/RootLayout.tsx
@@ -2,8 +2,21 @@ import { Link, Outlet } from "react-router-dom"
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, navigationMenuTriggerStyle } from "../components/ui/navigation-menu"
 import Header from "../components/ui/header";
 import { Archive, Bell, Lightbulb, Trash2 } from "lucide-react";
+import { useState } from "react";
 
 const RootLayout = () => {
+  const [selectedItem, setSelectedItem] = useState(0); // Initially selecting the first item
+
+  const navigationItems = [
+    { id: 0, label: 'Notes', link: '/Notes', icon: <Lightbulb className="p-2" size={40} /> },
+    { id: 1, label: 'Reminder', link: '/Reminder', icon: <Bell className="p-2" size={40} /> },
+    { id: 2, label: 'Archive', link: '/Archive', icon: <Archive className="p-2" size={40} /> },
+    { id: 3, label: 'Trash', link: '/Trash', icon: <Trash2 className="p-2" size={40} /> },
+  ];
+
+  const handleItemClick = (itemId: number) => {
+    setSelectedItem(itemId);
+  };
 
   return (
     <>
@@ -13,48 +26,24 @@ const RootLayout = () => {
         <div className="grid grid-cols-10">
           <div className="col-span-2">
             <NavigationMenu className="mt-2">
-              <NavigationMenuItem >
-                <Link to="/Notes">
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    <div className="flex flex-row">
-                      <Lightbulb className="p-2" size={40} />
-                      <div className="p-2 ml-2 text-base">Notes</div>
-                    </div>
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <Link to="/Reminder" >
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    <div className="flex flex-row">
-                      <Bell className="p-2" size={40} />
-                      <div className="p-2 ml-2 text-base">Reminder</div>
-                    </div>
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <Link to="/Archive" >
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    <div className="flex flex-row">
-                      <Archive className="p-2" size={40} />
-                      <div className="p-2 ml-2 text-base">Archive</div>
-                    </div>
 
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <Link to="/Trash" >
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    <div className="flex flex-row">
-                      <Trash2 className="p-2" size={40} />
-                      <div className="p-2 ml-2 text-base">Trash</div>
-                    </div>
+              {navigationItems.map((item) => {
+                return (
+                  <NavigationMenuItem key={item.id} onClick={() => handleItemClick(item.id)}>
+                    <Link to={`${item.link}`}>
+                      <NavigationMenuLink className={`group inline-flex h-10 w-full items-center ${selectedItem == item.id ? 'bg-yellow-300' : ''}  rounded-r-3xl  px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-yellow-300 focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-yellow-300 data-[state=open]:bg-yellow-300`}>
 
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
+                        <div className="flex flex-row">
+                          {item.icon}
+                          <div className="p-2 ml-2 text-base">{item.label}</div>
+                        </div>
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                )
+              })}
+
+
             </NavigationMenu>
           </div>
           <div className="col-span-8">
