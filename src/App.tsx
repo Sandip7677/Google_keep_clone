d18dd80a2a332c32a541433a19d062bf5429aca7
Sandip7677 <anuj7870040353@gmail.com>
2023-08-28 12:31:19 +0530

    warning removed form app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import RouterPage from "./Pages/RouterPage";
 import { useEffect, useState } from "react";
-import { User, UserResponse } from "@supabase/supabase-js";
+import { User } from "@supabase/supabase-js";
 import LoginPage from "./Pages/LoginPage";
 import { getUser } from "./Auth/loginApi";
 import { useQuery } from "react-query";
@@ -12,16 +12,7 @@ import { useQuery } from "react-query";
 function App() {
   const [user, setUser] = useState<User | null>();
   const { data } = useQuery('user', getUser);
-  // useEffect(() => {
-  //   const getuser = async () => {
-  //     const userData = await getUser();
-  //     setUser(userData);
-  //   }
-  //   getuser();
-  //   // if (user != null && user != undefined) {
-  //   //   console.log(user.user_metadata.avatar_url, "user");
-  //   // }
-  // }, [])
+
   useEffect(() => {
     setUser(data);
   }, [data])
