1adcc3eaf302633dc3e9014286f9f6606c3783e7
Sandip7677 <anuj7870040353@gmail.com>
2023-08-31 16:23:29 +0530

    restore feature implimented and queryinvalidation for mutation delete implimented

--- src/components/ui/NoteItem.tsx
+++ src/components/ui/NoteItem.tsx
@@ -4,62 +4,74 @@ import { Archive, ArchiveRestore, Trash2 } from 'lucide-react';
 import { Button } from './button';
 import { deleteItem, updateItem } from '../../services/api';
 import { useMutation } from 'react-query';
+import { queryClient } from "../../main"
 
 const NoteItem = ({ value }: any) => {
     const [archtoogler, setArchtoogler] = useState(!(value.is_archived));
     const [deltoogler, setDeltoogler] = useState(!(value.is_deleted));
 
     const mutation = useMutation(updateItem);
-    const mutationDelete = useMutation(deleteItem);
+    const mutationDelete = useMutation(deleteItem, {
+        onSettled: () => {
+            queryClient.invalidateQueries('deletednotes');
+        },
+    });
     const archivehandler = () => {
         const item = { is_archived: true }
         const id = value.id;
-        mutation.mutate({ item, id });
+        mutation.mutateAsync({ item, id });
     }
     const unarchivehandler = () => {
         const item = { is_archived: false }
         const id = value.id;
-        mutation.mutate({ item, id });
+        mutation.mutateAsync({ item, id });
     }
     const deleteHandler = () => {
         const item = { is_deleted: true }
         const id = value.id;
-        mutation.mutate({ item, id });
+        mutation.mutateAsync({ item, id });
     }
 
     const onDelete = () => {
         const id = value.id;
-        mutationDelete.mutate(id);
+        mutationDelete.mutateAsync(id);
     }
     const onRestore = () => {
-
+        const item = { is_deleted: false }
+        const id = value.id;
+        mutation.mutateAsync({ item, id });
     }
     return (
         <>
             <Card className="min-width-card min-height-card hover:shadow-lg">
-                <CardContent className='text-lg'>
-                    {value.note}
+                {mutationDelete.isLoading ? <div className="h-full w-full flex justify-center items-center text-xl font-bold">
+                    Deleting...
+                </div> : (
+                    <>
+                        <CardContent className='text-lg'>
+                            {value.note}
+                        </CardContent>
+                        <CardFooter className='items-center'>
+                            {value.reminder_time && <div className="p-1 border rounded mr-20">
+                                {value.reminder_time.substring(11, 19)}
+                            </div>}
+                            <div className="flex gap-x-5">
+                                {(value.is_deleted) == false &&
+                                    (archtoogler ?
+                                        (<Archive size={18} onClick={() => {
+                                            setArchtoogler(false);
+                                            archivehandler();
+                                        }} />) :
+                                        (<ArchiveRestore size={18} onClick={() => { setArchtoogler(true); unarchivehandler(); }} />))}
+                                {(value.is_deleted) == false && (deltoogler ? (<Trash2 size={18} onClick={() => { setDeltoogler(false); deleteHandler(); }} />) : (""))}
+                                {(value.is_deleted) == true && (<Button onClick={onDelete}>Delete</Button>)}
+                                {(value.is_deleted) == true && (<Button onClick={onRestore}>Restore</Button>)}
+                            </div>
 
-                </CardContent>
-                <CardFooter className='items-center'>
-                    {value.reminder_time && <div className="p-1 border rounded mr-20">
-                        {value.reminder_time.substring(11, 19)}
-                    </div>}
-                    <div className="flex gap-x-5">
-                        {(value.is_deleted) == false &&
-                            (archtoogler ?
-                                (<Archive size={18} onClick={() => {
-                                    setArchtoogler(false);
-                                    archivehandler();
-                                }} />) :
-                                (<ArchiveRestore size={18} onClick={() => { setArchtoogler(true); unarchivehandler(); }} />))}
-                        {(value.is_deleted) == false && (deltoogler ? (<Trash2 size={18} onClick={() => { setDeltoogler(false); deleteHandler(); }} />) : (""))}
-                        {(value.is_deleted) == true && (<Button onClick={onDelete}>Delete</Button>)}
-                        {(value.is_deleted) == true && (<Button onClick={onRestore}>Restore</Button>)}
-                    </div>
 
-
-                </CardFooter>
+                        </CardFooter>
+                    </>
+                )}
             </Card>
         </>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
